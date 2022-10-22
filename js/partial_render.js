44f8bb2014c040cbf4de4d83f122459eeb24c95c
Pedro <jpedro.catalano@gmail.com>
2022-10-22 16:32:39 -0300

    agregado resto de las paginas al partial render

--- js/partial_render.js
+++ js/partial_render.js
@@ -19,5 +19,29 @@ function iniciarPagina(){
             .catch(document.querySelector(".container").innerHTML = "se ha producido un error");
         })
     }
+    function cargarCrash(){
+        fetch("../crash.html")
+            .then(response => {response.text()
+                .then(text => {if (response.ok){
+                    document.querySelector(".container").innerHTML = text
+                } else {
+                    document.querySelector(".container").innerHTML = "se ha producido un error"
+                }
+            })
+            .catch(document.querySelector(".container").innerHTML = "se ha producido un error");
+        })
+    }
+    function cargarCrashBash(){
+        fetch("../crashbash.html")
+            .then(response => {response.text()
+                .then(text => {if (response.ok){
+                    document.querySelector(".container").innerHTML = text
+                } else {
+                    document.querySelector(".container").innerHTML = "se ha producido un error"
+                }
+            })
+            .catch(document.querySelector(".container").innerHTML = "se ha producido un error");
+        })
+    }
     
 }
