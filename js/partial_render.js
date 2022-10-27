b85ecc28c89306d2f3b1f817c512edef6d41927f
Pedro <jpedro.catalano@gmail.com>
2022-10-26 22:17:19 -0300

    agregado pagina de pepsiman al partial render

--- js/partial_render.js
+++ js/partial_render.js
@@ -6,7 +6,8 @@ function iniciarPagina(){
     document.getElementById("home").addEventListener("click", cargarHome);
     document.getElementById("crash").addEventListener("click", cargarCrash);
     document.getElementById("crashbash").addEventListener("click", cargarCrashBash);
-    
+    document.getElementById("pepsi-man").addEventListener("click", cargarPepsiman);
+
     function cargarHome(){
         fetch("../home.html")
             .then(response => {response.text()
@@ -43,5 +44,16 @@ function iniciarPagina(){
             .catch(document.querySelector(".container").innerHTML = "se ha producido un error");
         })
     }
-    
+    function cargarPepsiman(){
+        fetch("../pepsiman.html")
+            .then(response => {response.text()
+                .then(text => {if (response.ok){
+                    document.querySelector(".container").innerHTML = text
+                } else {
+                    document.querySelector(".container").innerHTML = "se ha producido un error"
+                }
+            })
+            .catch(document.querySelector(".container").innerHTML = "se ha producido un error");
+        })
+    }
 }
